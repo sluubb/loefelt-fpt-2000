46f01ff11bf8298f52823f47369b773f674a68ae
SlubblesThuggy <65078309+SlubblesThuggy@users.noreply.github.com>
2025-07-30 14:29:31 +0200

    feat: improve debugability

--- probe/probe.ino
+++ probe/probe.ino
@@ -1,30 +1,38 @@
 #include <SPI.h>
 #include <RH_RF95.h>
 
-#define SERIAL_DEBUG
-
+#define DEBUG 26
 #define STATUS_LED 13
+
+bool debugMode = false;
 
 // LoRa module
 #define LORA_CS 1
 #define LORA_INT 24
 #define LORA_RST 25
-const float loraFrequency = 900.0; // MHz
+const float loraFrequency = 433.0; // MHz
 const int loraBandwidth = 125;     // kHz
-const int loraTxPower = 13;        // dBm
+const int loraTxPower = 20;        // dBm
 RH_RF95 lora(LORA_CS, LORA_INT);
 uint8_t buf[RH_RF95_MAX_MESSAGE_LEN]; 
 
 void setup() {
+    pinMode(DEBUG, INPUT_PULLUP);
     pinMode(STATUS_LED, OUTPUT);
 
     pinMode(LORA_RST, OUTPUT);
     digitalWrite(LORA_RST, HIGH);
 
-#ifdef SERIAL_DEBUG
-    Serial.begin(9600);
-    while (!Serial);
-#endif
+    if (digitalRead(DEBUG) == HIGH) {
+	debugMode = true;
+
+	Serial.begin(9600);
+	while (!Serial);
+
+	Serial.println("DEBUG MODE ENABLED");
+    }
+
+    delay(1000);
 
     digitalWrite(LORA_RST, LOW);
     delay(10);
@@ -46,24 +54,26 @@ void loop() {
 }
 
 void log(String msg) {
-    lora.waitPacketSent();
-
     int len = msg.length();
     msg.toCharArray((char *)buf, sizeof(buf));
 
     lora.send(buf, len);
+
+    digitalWrite(STATUS_LED, HIGH);
+    delay(10);
+    digitalWrite(STATUS_LED, LOW);
 }
 
 void error(String msg) {
-#ifdef SERIAL_DEBUG
-    Serial.print("ERROR :: ");
-    Serial.println(msg);
-#endif
+    if (debugMode) {
+	Serial.print("ERROR :: ");
+	Serial.println(msg);
+    }
 
     while (1) {
 	digitalWrite(STATUS_LED, HIGH);
-	delay(100);
+	delay(500);
 	digitalWrite(STATUS_LED, LOW);
-	delay(900);
+	delay(500);
     }
 }
--- station/station.ino
+++ station/station.ino
@@ -5,9 +5,8 @@
 #define LORA_CS 1
 #define LORA_INT 24
 #define LORA_RST 25
-const float loraFrequency = 900.0; // MHz
+const float loraFrequency = 433.0; // MHz
 const int loraBandwidth = 125;     // kHz
-const int loraTxPower = 13;        // dBm
 RH_RF95 lora(LORA_CS, LORA_INT);
 uint8_t buf[RH_RF95_MAX_MESSAGE_LEN]; 
 
@@ -17,34 +16,33 @@ void setup() {
 
     Serial.begin(9600);
     while (!Serial);
+    delay(1000);
 
     digitalWrite(LORA_RST, LOW);
     delay(10);
     digitalWrite(LORA_RST, HIGH);
     delay(10);
 
-    if (!lora.init()) {
+    while (!lora.init()) {
 	Serial.println("Failed to init LoRa");
-	while (1);
+	delay(1000);
     }
 
     lora.setFrequency(loraFrequency);
     lora.setSignalBandwidth(loraBandwidth);
-    lora.setTxPower(loraTxPower, false);
 }
 
 void loop() {
-    if (lora.available()) {
-	uint8_t len = sizeof(buf);
-	if (lora.recv(buf, &len)) {
-	    RH_RF95::printBuffer("Received: ", buf, len);
-	    Serial.print("Got: ");
-	    Serial.println((char*)buf);
-	    Serial.print("RSSI: ");
-	    Serial.println(lora.lastRssi(), DEC);
-	} else {
-	    Serial.println("Receive failed");
-	}
+    Serial.println("Waiting for receive...");
+    lora.waitAvailable();
+    uint8_t len = sizeof(buf);
+    if (lora.recv(buf, &len)) {
+	Serial.print("Got: ");
+	Serial.println((char *)buf);
+	Serial.print("RSSI: ");
+	Serial.println(lora.lastRssi(), DEC);
+    } else {
+	Serial.println("Receive failed");
     }
 }
 
